52f178361e23f6d55e8bf46b624c6e8f5fb8d029
Ozan <ozansahin92@gmail.com>
2020-10-10 20:12:18 +0200

    django additions and json additions to webscarper

--- utils/webScraper.py
+++ utils/webScraper.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
+import json
 
 URL = 'https://www.germanplates.com/german-license-plate-codes'
 page = requests.get(URL)
@@ -9,5 +10,11 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 results = soup.find_all('p')
 
+vrpCodesGermany = {}
+
 for result in results:
-    print(result.text)
+    if len(result.text.split('-', 1)) >= 2:
+        vrpCodesGermany[result.text.split('-', 1)[0]] = result.text.split('-', 1)[1]
+
+with open('vrpCodesGermany.json', 'w') as outfile:
+    json.dump(vrpCodesGermany, outfile)
